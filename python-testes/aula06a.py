0302d987f5f6e0e4acdb3ff81f2a0a3fd028f3b3
Anndersom Naum <a.naumdev@gmail.com>
2026-01-16 22:21:41 -0300

    atualizando os exercicios

--- python-testes/aula06a.py
+++ python-testes/aula06a.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro: '))
 s = n1 + n2
-print('A soma entre {} e {} é {}'.format(n1,n2,s))
+print('A soma entre {2} e {0} vale {1}'.format(n1,n2,s))
