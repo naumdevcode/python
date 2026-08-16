99659b8b880fe36891583ae97b180a9c33b6d4bd
Anndersom Naum <a.naumdev@gmail.com>
2026-08-16 12:23:51 -0300

    correção exercicio

--- python-exercicios/ex057.py
+++ python-exercicios/ex057.py
@@ -1,6 +1,8 @@
-sexo = ''
-while sexo not in 'MF' or sexo == '':
-    sexo = str(input('Qual o seu sexo [M/F]: ')).strip().upper()[0]
-    if sexo not in 'MF' or sexo == '':
-        print('Sexo inválido, tente novamente')
+sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dados inválido. Por favor, informe seu sexo: ')).strip().upper()[0]
+if sexo == 'M':
+    sexo = 'Masculino'
+elif sexo == 'F':
+    sexo = 'Feminino'
 print(f'Sexo {sexo} registrado com sucesso!')
